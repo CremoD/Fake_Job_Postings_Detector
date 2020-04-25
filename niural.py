48a82921e93f713a602ee84481f44ce6134fc0dd
Sbetti Davide (Student Com16) <davide.sbetti@stud-inf.unibz.it>
2020-04-25 12:58:16 +0200

    niural2

--- niural.py
+++ niural.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Niural:
 
 	def __init__(self, n_epochs = 10, learning_rate = 0.001, max_iter= 100, hidden_layer = [100]):
@@ -10,18 +12,64 @@ class Niural:
 		print("Number of epochs", self.n_epochs)
 		print("learning_rate", self.learning_rate)
 		print("max_iter", self.max_iter)
-
-
-
-	def random(self, n_features, n_classes, hidden_layer):
-		n_hidden_layer = hidden_layer.length
-		self.weights = []
-
-		for i in range(0, n_hidden_layer+2):
-
-
+		print("Fitting the data")
 		
 
 	def fit(self, X, y):
-		n_classes = y.nunique()
-		random(X.shape[1], n_classes, self.hidden_layer)
+		n_classes = y.nunique()[0]
+		
+		if(n_classes == 2):
+			n_classes = 1
+
+		self.random_weights(X.shape[1], n_classes, self.hidden_layer)
+
+		print("Weights")
+		for mat in self.weights:
+			print(mat)
+			print("-------------------------")
+
+		res = self.forward_propagate(X.loc[0,:])
+			
+		print("Prediction is:")
+		print(res)
+
+		train_errors = self.layers_errors(res,np.array(y.loc[0,:]))
+
+			
+		print("-------------------------")
+		print(train_errors)
+
+	def random_weights(self, n_features, n_classes, hidden_layer):
+		self.weights = []
+		
+		units = [n_features] + hidden_layer + [n_classes]
+
+		for i in range(0, len(units) - 1):
+			self.weights.append(np.random.rand(units[i+1],units[i] + 1))
+
+	def forward_propagate(self, x):
+		a = x.to_numpy()
+		a_history = [a]
+		for theta in self.weights:
+			a = np.insert(a,0,1)
+			z = np.dot(a,np.transpose(theta))
+			a = self.sigmoid(z)
+			a_history.append(a)
+
+		return a_history
+
+	def layers_errors(self,a,y):
+		# calculate error for output layer
+		errors = [(a[-1] - y).tolist()]
+
+		i = len(self.hidden_layer)
+		while i >= 1:
+			print("Looping over",i)
+			current_error = np.dot(np.transpose(self.weights[i]),np.array([errors[0]])) * (a[i] * (1 - a[i]))
+			errors.insert(0,current_error.tolist())
+			i = i - 1
+
+		return errors
+
+	def sigmoid(self,number):
+		return 1/(1 + np.exp(-number))
